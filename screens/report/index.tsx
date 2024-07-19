c809d9d20b68c2aee494be6ea619e5e91f438fd9
hainm149889 <hainm149889@gmail.com>
2024-07-19 15:38:34 +0700

    update header report agent

--- screens/report/index.tsx
+++ screens/report/index.tsx
@@ -3,15 +3,19 @@ import { navigate } from '@navigation/navigation-service';
 import { createStyleSheet, useStyles } from '@theme';
 import { APP_SCREEN } from '@utils';
 import {
+  Block,
   Button,
+  Icon,
   Image,
   NormalHeaderGradient,
   Screen,
+  Text,
 } from '@vna-base/components';
 import { scale } from '@vna-base/utils';
 import React from 'react';
 import { ScrollView } from 'react-native';
 import { EmployeeTurnover, Profit, Revenue, SalesReport } from './components';
+import dayjs from 'dayjs';
 
 export const Report = () => {
   const { styles } = useStyles(styleSheet);
@@ -38,6 +42,42 @@ export const Report = () => {
           />
         }
       />
+      <Block
+        paddingVertical={8}
+        paddingHorizontal={12}
+        borderBottomWidth={10}
+        borderColorTheme="neutral50"
+        flexDirection="row"
+        colorTheme="neutral100"
+        alignItems="center"
+        justifyContent="space-between">
+        <Block flexDirection="row" alignItems="center" columnGap={8}>
+          <Block padding={2}>
+            <Icon icon="calendar_fill" size={24} colorTheme="neutral80" />
+          </Block>
+          <Text
+            text={`${dayjs()
+              .subtract(7, 'day')
+              .format('DD/MM/YYYY')} - ${dayjs().format('DD/MM/YYYY')}`}
+            fontStyle="Body14Reg"
+            colorTheme="neutral80"
+          />
+        </Block>
+        <Block
+          flexDirection="row"
+          alignItems="center"
+          columnGap={8}
+          borderLeftWidth={10}
+          borderColorTheme="neutral50"
+          paddingLeft={8}>
+          <Text
+            text="Tất cả đại lý"
+            fontStyle="Body14Reg"
+            colorTheme="neutral80"
+          />
+          <Icon icon="arrow_ios_down_fill" size={24} colorTheme="neutral700" />
+        </Block>
+      </Block>
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <SalesReport />
         <Revenue />
